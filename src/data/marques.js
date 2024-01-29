f24aeaec2d4261bdf6c1277d1bfe44dae70a991e
ricard-gi <ricard.hernandez.gi@gmail.com>
2024-01-30 00:41:26 +0100

    marques2

---
+++ src/data/marques.js
@@ -0,0 +1,46 @@
+
+export default [
+    { logo: "aprilia.jpg", 
+        marca: "APRILIA" },
+    {
+        logo: "ducati.jpg",
+        marca: "DUCATI",
+    },
+    {
+        logo: "gilera.png",
+        marca: "GILERA",
+    },
+    {
+        logo: "honda.jpg",
+        marca: "HONDA",
+    },
+    {
+        logo: "kymco.jpg",
+        marca: "KYMCO",
+    },
+    {
+        logo: "peugeot.jpg",
+        marca: "PEUGEOT",
+    },
+    {
+        logo: "piaggio.jpg",
+        marca: "PIAGGIO",
+    },
+    {
+        logo: "suzuki.jpg",
+        marca: "SUZUKI",
+    },
+    {
+        logo: "sym.jpg",
+        marca: "SYM",
+    },
+    {
+        logo: "vespa.jpg",
+        marca: "VESPA",
+    },
+    {
+        logo: "yamaha.jpg",
+        marca: "YAMAHA",
+    },
+];
+
